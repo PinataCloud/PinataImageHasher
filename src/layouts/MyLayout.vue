2c617c0a3e53fdc5d0fbf0d7916ba0763fa9d5ef
Dan Shields <danwshields@gmail.com>
2019-10-25 16:17:27 -0600

    branding

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -13,8 +13,8 @@
         />
 
         <q-toolbar-title v-if="$q.screen.gt.sm" shrink class="row items-center no-wrap">
-          <img src="https://cdn.quasar.dev/img/layout-gallery/logo-google.svg">
-          <span class="q-ml-sm">Photos</span>
+          <img src="~assets/corraborator.png"  height="20">
+            <span class="q-ml-sm"><strong>Corro</strong>borator</span>
         </q-toolbar-title>
 
         <q-space />
@@ -47,9 +47,6 @@
         <q-space />
 
         <div class="q-gutter-sm row items-center no-wrap">
-          <q-btn round dense flat color="text-grey-7" icon="apps">
-            <q-tooltip>Google Apps</q-tooltip>
-          </q-btn>
           <q-btn round dense flat color="grey-8" icon="notifications">
             <q-badge color="red" text-color="white" floating>
               2
@@ -75,6 +72,7 @@
       <q-scroll-area class="fit">
         <q-toolbar class="GPL__toolbar">
           <q-toolbar-title class="row items-center text-grey-8">
+            <img src="~assets/corraborator.png" height="20">
             <span class="q-ml-sm"><strong>Corro</strong>borator</span>
           </q-toolbar-title>
         </q-toolbar>
@@ -150,15 +148,6 @@
             </q-badge>
           </q-btn>
 
-          <q-btn round flat color="grey-8" stack no-caps size="26px" class="GPL__side-btn">
-            <q-icon size="22px" name="group" />
-            <div class="GPL__side-btn__label">Sharing</div>
-          </q-btn>
-
-          <q-btn round flat color="grey-8" stack no-caps size="26px" class="GPL__side-btn">
-            <q-icon size="22px" name="import_contacts" />
-            <div class="GPL__side-btn__label">Photo books</div>
-          </q-btn>
         </div>
       </q-page-sticky>
     </q-page-container>
